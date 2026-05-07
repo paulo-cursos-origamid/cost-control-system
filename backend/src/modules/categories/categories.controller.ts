226be66f1a80cc0ac842f252998c63d30cb346d9
CURSOS ORIGAMID <paulocursosorigamid@gmail.com>
2026-05-06 22:59:24 -0300

    chore: initial backend architecture refactor .controllerss

--- backend/src/modules/categories/categories.controller.ts
+++ backend/src/modules/categories/categories.controller.ts
@@ -1,4 +1,7 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
 
+import { JwtAuthGuard } from '../../common/guards/jwt-auth.guard';
+
+@UseGuards(JwtAuthGuard)
 @Controller('categories')
 export class CategoriesController {}
