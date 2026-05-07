d3be3ab504ca19004e64a2a240c423d63010a784
CURSOS ORIGAMID <paulocursosorigamid@gmail.com>
2026-05-06 22:48:39 -0300

    chore: initial backend architecture refactor register on auth.module.tss

--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
+
+import { PassportModule } from '@nestjs/passport';
+
+import { JwtModule } from '@nestjs/jwt';
+
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService]
+  imports: [
+    PassportModule,
+
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+
+      signOptions: {
+        expiresIn: '1d',
+      },
+    }),
+  ],
+
+  providers: [JwtStrategy],
+
+  exports: [JwtModule, PassportModule],
 })
 export class AuthModule {}
