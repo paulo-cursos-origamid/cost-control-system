c641e76266c19a6ac64edd7562ef8da3164fd39e
CURSOS ORIGAMID <paulocursosorigamid@gmail.com>
2026-05-08 11:49:27 -0300

    refactor(architecture): reorganize backend structure

---
+++ backend/src/common/decorators/current-user.decorator.ts
@@ -0,0 +1,20 @@
+import {
+  createParamDecorator,
+  ExecutionContext,
+} from '@nestjs/common';
+
+import { JwtUser } from '..';
+
+export const CurrentUser =
+  createParamDecorator(
+    (
+      _data: unknown,
+      ctx: ExecutionContext,
+    ): JwtUser => {
+      const request = ctx
+        .switchToHttp()
+        .getRequest();
+
+      return request.user as JwtUser;
+    },
+  );
